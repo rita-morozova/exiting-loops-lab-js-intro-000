df37ca12b87211192846e5ee2867e31cc1569e1d
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-27 17:54:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ function breakOut(array, changeValue, stopValue) {
   return array
 }
 
+function keepGoing(array, changeValue, skipValue) {
+  for (var i = 0; i<array.length; i++) {
+    if (array[i] === skipValue) {
+      continue
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+
+
